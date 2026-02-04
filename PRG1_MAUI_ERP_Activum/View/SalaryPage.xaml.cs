d5ecb28103420a7e0dbc04d35b9936e6c14a4ff4
Kristoffer <kristoffer.wahlback@elev.ga.lbs.se>
2026-02-04 13:18:22 +0100

    Implementera lönekalkylator med svensk valuta (Ref #4)

--- PRG1_MAUI_ERP_Activum/View/SalaryPage.xaml.cs
+++ PRG1_MAUI_ERP_Activum/View/SalaryPage.xaml.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace PRG1_MAUI_ERP_Activum.View;
 
 public partial class SalaryPage : ContentPage
@@ -6,4 +8,28 @@ public partial class SalaryPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private void OnCalculateSalaryClicked(object sender, EventArgs e)
+	{
+		if (double.TryParse(SalesEntry.Text, out double sales))
+		{
+			double baseSalary = 26000;
+			double commission = sales * 0.12;
+			double grossSalary = baseSalary + commission;
+
+			double tax = grossSalary * 0.32;
+			double netSalary = grossSalary - tax;
+
+			var swedishCulture = new CultureInfo("sv-SE");
+
+			CommissionLabel.Text = commission.ToString("C0", swedishCulture);
+			TaxLabel.Text = tax.ToString("C0", swedishCulture);
+			NetSalaryLabel.Text = netSalary.ToString("C0", swedishCulture);
+		}
+		else
+		{
+			DisplayAlert("Fel", "Skriv in en siffra för försäljningen.", "OK");
+		}
+	}
+
 }
